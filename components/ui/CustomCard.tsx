c975162ae6b80c26292438010bd8eb31513af15a
itamim-coder <itamim12202@gmail.com>
2025-01-07 23:40:48 +0600

    [CONFIGURE]

--- components/ui/CustomCard.tsx
+++ components/ui/CustomCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Image, View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Card, Title, Paragraph } from "react-native-paper";
-
+import { Image } from "expo-image";
 interface CustomCardProps {
   name: string;
   description?: string;
@@ -21,8 +21,16 @@ const CustomCard: React.FC<CustomCardProps> = ({
 }) => {
   return (
     <Card className={`rounded-lg   shadow-lg ${className}`}>
-      <Image width={120} height={150} source={{ uri: image }} />
-
+      {/* <Image width={120} height={150} source={{ uri: image }} /> */}
+      <Image
+        // width={120}
+        // height={150}
+        className="w-36 h-40"
+        source={image}
+        // placeholder={{ blurhash }}
+        contentFit="cover"
+        transition={1000}
+      />
       <Title className="text-sm my-2 font-semibold text-center">{name}</Title>
       {/* <Paragraph className="mb-4 text-gray-600">{description}</Paragraph> */}
     </Card>
